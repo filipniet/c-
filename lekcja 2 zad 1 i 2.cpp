74712d4a219effd8e43896b8d0ab8c7bb717ce59
filipniet <90102642+filipniet@users.noreply.github.com>
2021-09-19 13:01:53 +0200

    Add files via upload

---
+++ lekcja 2 zad 1 i 2.cpp
@@ -0,0 +1,23 @@
+﻿// lekcja 2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int tab[10][10];
+    
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tab[i][j] = 0;
+        }
+    }
+    for (int a = 0; j < 10; j++) {
+        for (int b = 0; j < 10; j++) {
+            cout << tab[a][b];
+        }
+        endl;
+    }
+
+    
+}
---
+++ lekcja 2 zad 3.cpp
@@ -0,0 +1,26 @@
+﻿// lekcja 2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int tab[10][10];
+    
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tab[i][j] = 0;
+        }
+    }
+    for (int p = 0; p < 10; p++) {
+        tab[p][p] = 1;
+    }
+    for (int a = 0; a < 10; a++) {
+        for (int b = 0; b < 10; b++) {
+            cout << tab[a][b];
+        }
+        cout << endl;
+    }
+
+    
+}
